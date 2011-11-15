82f8a60ae9cc3591afedbbf682974d76044ef3f5
Tewfik Sadaoui <tewfik.sadaoui@gmail.com>
2011-11-15 04:39:00 +0100

    The controller parse the TSC JSON content.

--- src/ControllerBehaviour.java
+++ src/ControllerBehaviour.java
@@ -1,13 +1,13 @@
 /*
  * TODO: 
- * - parse json_content
  * - determine which action to execute
  * - send message to actuator
- *
+ * - optimisation: reuse the same json_parser, don't recreate it
  */
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
@@ -15,11 +15,26 @@ import jade.lang.acl.ACLMessage;
 
 public class ControllerBehaviour extends CyclicBehaviour {
     public void action() {
+	/* receive the message */
 	ACLMessage msg = this.myAgent.receive();
 	if (msg != null) {
-	    String json_content = msg.getContent();
+	    /* parse the json message */
 
-	    System.out.println("Controller: " + json_content);
+	    // to optimize performance we can avoid this object
+	    // creation and reuse an existing one.
+	    JSONParser parser = new JSONParser();
+	    Object parsed_content = null;
+
+	    try {
+		parsed_content = (JSONObject)parser.parse(msg.getContent());
+	    } catch(ParseException e) {
+		System.out.println("Controller:error:" + e);
+	    }
+
+	    JSONObject json_content = (JSONObject)parsed_content;
+
+	    /* access and process the received values from the sensor */
+	    System.out.println("Controller: pv = " + json_content.get("pv"));
 	}
 	else {
 	    block();
